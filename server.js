ddc7eaefa014d75bcdf2df3e2fd384b8b9b261f6
cpichac <Carlos12pichardo@outlook.com>
2023-09-05 19:32:14 -0400

    saving files

---
+++ server.js
@@ -0,0 +1,40 @@
+// Import required modules and packages
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection'); // Your Sequelize configuration
+const routes = require('./controllers'); // Your routes
+
+// Create an instance of the Express.js application
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to handle POST data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up sessions for user authentication
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Set up Handlebars.js as the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Serve static assets from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use the defined routes from your controllers
+app.use(routes);
+
+// Start the Sequelize database connection
+sequelize.sync({ force: false }).then(() => {
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
